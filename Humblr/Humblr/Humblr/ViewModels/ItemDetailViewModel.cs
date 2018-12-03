1aa8c3574d3f97f542124fe466b2a5a29c9ac183
Sekrai <andre.dilles@hotmail.com>
2018-12-03 23:53:57 +0100

    First Commit
    First!

---
+++ Humblr/Humblr/Humblr/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Humblr.Models;
+
+namespace Humblr.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
